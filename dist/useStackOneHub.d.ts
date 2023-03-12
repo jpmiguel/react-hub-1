29090d45b141332e7c822be9c390f6c8334d31a2
Guillaume Lebedel <guillaume.lebedel@gmail.com>
2023-03-12 03:23:44 +0000

    ci: remove dist folder

--- dist/useStackOneHub.d.ts
+++
@@ -1,8 +0,0 @@
-import { StartOptions } from './entities/StartOptions';
-declare function useStackOneHub({ connectUrl }?: {
-    connectUrl?: string;
-}): {
-    startConnect: (options: StartOptions) => void;
-};
-export { useStackOneHub };
-//# sourceMappingURL=useStackOneHub.d.ts.map
